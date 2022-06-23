7ce3a71a0b249a0bdd1ca037887a71fbcdec24c9
kibeshev87 <mrjimb5@gmail.com>
2022-06-24 00:37:23 +0300

    wednesday_4_lesson

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -1,6 +1,6 @@
 import React, {useCallback} from 'react';
 import './App.css';
-import {TaskType, Todolist} from './Todolist';
+import {TaskType, Todolist1} from './Todolist1';
 import {v1} from 'uuid';
 import {AddItemForm} from './AddItemForm';
 import {
@@ -27,11 +27,14 @@ export type TasksStateType = {
 
 
 function AppWithRedux() {
+
     let todolistId1 = v1();
     let todolistId2 = v1();
 
     let todolists = useSelector<AppRootStateType, Array<TodolistType>>(state => state.todolists)
     let tasks = useSelector<AppRootStateType, TasksStateType>(state => state.tasks)
+
+
 
     const dispatch = useDispatch()
 
@@ -85,20 +88,20 @@ function AppWithRedux() {
                     //     tasksForTodolist = allTodolistTasks.filter(t => t.isDone);
                     // }
 
-                    return <Todolist
+                    return <Todolist1
                         key={tl.id}
-                        //todolist={tl}//при использовании Todolist1
-                        id={tl.id}
-                        title={tl.title}
-                        tasks={tasks[tl.id]}
-                        removeTask={removeTask}
-                        changeFilter={changeFilter}
-                        addTask={addTask}
-                        changeTaskStatus={changeStatus}
-                        filter={tl.filter}
-                        removeTodolist={removeTodolist}
-                        changeTaskTitle={changeTaskTitle}
-                        changeTodolistTitle={changeTodolistTitle}
+                        todolist={tl}//при использовании Todolist1
+                        // id={tl.id}
+                        // title={tl.title}
+                        // tasks={tasks[tl.id]}
+                        // removeTask={removeTask}
+                        // changeFilter={changeFilter}
+                        // addTask={addTask}
+                        // changeTaskStatus={changeStatus}
+                        // filter={tl.filter}
+                        // removeTodolist={removeTodolist}
+                        // changeTaskTitle={changeTaskTitle}
+                        // changeTodolistTitle={changeTodolistTitle}
                     />
                 })
             }
